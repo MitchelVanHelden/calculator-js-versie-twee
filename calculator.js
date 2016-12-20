1091132f879aeb94e7a2f39c5d4bb251ae67dfb6
mitchel van helden <mitchel van helden>
2016-12-20 08:41:35 +0100

    -

---
+++ calculator.js
@@ -0,0 +1,32 @@
+var som = document.getElementById('som')
+
+
+function cijfer1(cijfer){
+	console.log(cijfer + " ");
+	som.innerHTML += cijfer;
+}
+
+
+
+
+
+
+/*
+//voorbeeld code
+//parameter
+function functienaam(piet){
+	console.log(piet + " strooit snoep");
+}
+
+functienaam("zwarte piet");
+
+var display= "p";
+
+function functienaamTwee(piet){
+	display = display+ piet;
+}
+
+console.log(display);
+functienaamTwee("i");
+console.log(display);
+*/
