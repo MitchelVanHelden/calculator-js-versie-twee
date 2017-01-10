17e2f6cab85c732938e5154bbc8ba16e45eaf554
mitchel van helden <mitchel van helden>
2017-01-10 13:50:58 +0100

    no message

--- calculator.js
+++ calculator.js
@@ -1,11 +1,9 @@
-var som = document.getElementById('som')
+function test(n1){
+	console.log(test)
+	calculator.txt1.value=calculator.txt1.value+n1;
+	
 
-
-function cijfer1(cijfer){
-	console.log(cijfer + " ");
-	som.innerHTML += cijfer;
 }
-
 
 
 
