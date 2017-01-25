35d0d51bc31142d884c0b3d160bb38f3455c41e8
mitchel van helden <mitchel van helden>
2017-01-25 09:39:24 +0100

    no message

--- calculator.js
+++ calculator.js
@@ -1,29 +1,4 @@
 function test(n1){
 	console.log(test)
 	calculator.txt1.value=calculator.txt1.value+n1;
-	
 }
-
-
-
-
-
-/*
-//voorbeeld code
-//parameter
-function functienaam(piet){
-	console.log(piet + " strooit snoep");
-}
-
-functienaam("zwarte piet");
-
-var display= "p";
-
-function functienaamTwee(piet){
-	display = display+ piet;
-}
-
-console.log(display);
-functienaamTwee("i");
-console.log(display);
-*/
